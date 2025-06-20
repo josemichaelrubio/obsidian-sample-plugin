400478f95b8e4aa54cb50e7ba5214871bc0415a3
josemichaelrubio <contact@josemichaelrubio.com>
2025-06-20 11:26:44 -0400

    Anatomy of a plugin: added console prints to onload() and onunload()

--- main.ts
+++ main.ts
@@ -16,6 +16,8 @@ export default class HelloWorldPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		console.log('loading plugin')
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
@@ -83,6 +85,8 @@ export default class HelloWorldPlugin extends Plugin {
 	}
 
 	onunload() {
+
+		console.log('unloading plugin')
 
 	}
 
